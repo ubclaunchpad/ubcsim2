b50a936ed528af503158fb7962c3295164769689
Robin Reyes <rmcreyes.eng@gmail.com>
2019-10-15 23:18:15 -0700

    Set up unit testing

---
+++ test/sample_test.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("Sample test", function() {
+  it("should be able to make basic assertions", function() {
+    expect("Hello".length).to.equal(5);
+  });
+});
