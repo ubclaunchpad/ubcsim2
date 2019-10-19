96e758ab0c6f730cf65da11e5627439ab10ea46e
Eloise Peng <eloise.k.p@gmail.com>
2019-10-19 15:26:28 -0700

    removed yarn.lock; update State class

--- modules/main.ts
+++ modules/main.ts
@@ -5,10 +5,22 @@ interface IEvent {
   updateEvents(state: State): void;
 }
 
+const DEFAULT_SLEEP = 100;
+
 class State {
-  public set: Set<IEvent>;
+  public pool: Set<IEvent>;
   public queue: Array<IEvent>;  // Queue<IEvent>
   public friends: number;
   public gpa: number;
   public sleep: number;
+  public week: number;
+  
+  constructor(friends: number, gpa: number) {
+    this.pool = new Set;
+    this.queue = new Array;
+    this.friends = friends;
+    this.gpa = gpa;
+    this.sleep = DEFAULT_SLEEP;
+    this.week = 0;
+  }
 }
