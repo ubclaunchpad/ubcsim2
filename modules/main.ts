510c2bcee0a9f86c88ec27ce41e3f132423eef95
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2019-10-28 23:12:56 -0700

    #13

--- modules/main.ts
+++ modules/main.ts
@@ -1,26 +1,36 @@
 interface IEvent {
-  displayPrompt(): void;
-  getInput(): void;
-  displayResponse(): void;
-  updateEvents(state: State): void;
+    displayPrompt(): void;
+
+    getInput(): void;
+
+    displayResponse(): void;
+
+    updateEvents(state: State): void;
 }
 
 const DEFAULT_SLEEP = 100;
 
 class State {
-  public pool: Set<IEvent>;
-  public queue: Array<IEvent>;  // Queue<IEvent>
-  public friends: number;
-  public gpa: number;
-  public sleep: number;
-  public week: number;
-  
-  constructor(friends: number, gpa: number) {
-    this.pool = new Set;
-    this.queue = new Array;
-    this.friends = friends;
-    this.gpa = gpa;
-    this.sleep = DEFAULT_SLEEP;
-    this.week = 0;
-  }
+    public pool: IEvent[];
+    public queue: IEvent[];  // Queue<IEvent>
+    public friends: number;
+    public gpa: number;
+    public sleep: number;
+    public week: number;
+
+    constructor(friends: number, gpa: number) {
+        this.pool = [];
+        this.queue = [];
+        this.friends = friends;
+        this.gpa = gpa;
+        this.sleep = DEFAULT_SLEEP;
+        this.week = 0;
+    }
+
+    public getRandomEvent(): IEvent {
+        if (this.queue.length !== 0) {
+            return this.queue.shift();
+        }
+        return this.pool[Math.floor(Math.random() * this.pool.length)];
+    }
 }
