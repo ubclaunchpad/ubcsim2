dafbfc6cf604826882464402ea940fb8870e59cb
Eloise Peng <eloise.k.p@gmail.com>
2019-11-02 00:16:36 -0700

    moved .ts files to /src

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, world!");
+interface IEvent {
+  displayPrompt(): void;
+  getInput(): void;
+  displayResponse(): void;
+  updateEvents(state: State): void;
+}
+
+const DEFAULT_SLEEP = 100;
+
+class State {
+  public pool: Set<IEvent>;
+  public queue: IEvent[];  // Queue<IEvent>
+  public friends: number;
+  public gpa: number;
+  public sleep: number;
+  public week: number;
+
+  constructor(friends: number, gpa: number) {
+    this.pool = new Set();
+    this.queue = [];
+    this.friends = friends;
+    this.gpa = gpa;
+    this.sleep = DEFAULT_SLEEP;
+    this.week = 0;
+  }
+}
