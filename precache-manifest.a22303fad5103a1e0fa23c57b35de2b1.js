04547d5a83ad8572f7e0b79cf96dbe83c5a30e6f
Eloise Peng <eloise.k.p@gmail.com>
2019-11-16 11:51:19 -0800

    Updates

---
+++ precache-manifest.a22303fad5103a1e0fa23c57b35de2b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0180d0678595d321562fcc15e9b262",
+    "url": "/ubcsim2/index.html"
+  },
+  {
+    "revision": "1fdc1f62f9d9e4e202f9",
+    "url": "/ubcsim2/static/js/2.acac45af.chunk.js"
+  },
+  {
+    "revision": "aae8f6fbe09ab04e105b",
+    "url": "/ubcsim2/static/js/main.fbdbc551.chunk.js"
+  },
+  {
+    "revision": "1871cdf7e34339ebcc7c",
+    "url": "/ubcsim2/static/js/runtime-main.d46f3c7c.js"
+  }
+]);
