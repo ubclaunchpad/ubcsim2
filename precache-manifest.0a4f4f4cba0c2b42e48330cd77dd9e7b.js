4d10d210a1dd5ef4a53621bebbab67456b0625fe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-16 22:34:45 +0000

    Deploy ubclaunchpad/ubcsim2 to github.com/ubclaunchpad/ubcsim2.git:gh-pages

---
+++ precache-manifest.0a4f4f4cba0c2b42e48330cd77dd9e7b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf099dbfa2a1ccf41245a6d7d5e213e",
+    "url": "/ubcsim2/index.html"
+  },
+  {
+    "revision": "1fdc1f62f9d9e4e202f9",
+    "url": "/ubcsim2/static/js/2.acac45af.chunk.js"
+  },
+  {
+    "revision": "6a1bc7b5c0fe599432c1",
+    "url": "/ubcsim2/static/js/main.a0482989.chunk.js"
+  },
+  {
+    "revision": "1871cdf7e34339ebcc7c",
+    "url": "/ubcsim2/static/js/runtime-main.d46f3c7c.js"
+  }
+]);
