3cd692a4827915e0a8b7fc5f03e2b734bb6e6c0d
Robin Reyes <rmcreyes.eng@gmail.com>
2019-11-17 04:46:11 -0800

    Touch up App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,11 +4,7 @@ import App from "../src/App";
 
 // sample test
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it("should be able to make basic assertions", () => {
-  expect("Hello".length).toEqual(5);
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
