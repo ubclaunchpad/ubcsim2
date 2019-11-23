51e01399447f3eccb277aeb7626831de0ec6eeb1
Eloise Peng <32313451+eloisepeng@users.noreply.github.com>
2019-11-22 22:48:14 -0800

    Update src/components/NameIndicator.tsx
    Co-Authored-By: Roberto Martin Reyes <rmcreyes.eng@gmail.com>

--- src/components/NameIndicator.tsx
+++ src/components/NameIndicator.tsx
@@ -4,6 +4,6 @@ interface IProps {
     name: string;
 }
 
-export default function TimeIndicator(props: IProps) {
+export default function NameIndicator(props: IProps) {
     return <span id="name" className="this-align-left column float-left">Name: {props.name}</span>;
 }
