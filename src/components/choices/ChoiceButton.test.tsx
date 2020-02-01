40a15c61714bd9fcb7d4fb13fe68079454b83f28
Cheuk Yin Ng <chucksys88@gmail.com>
2020-02-01 15:53:17 -0800

    Add choicebutton render test

---
+++ src/components/choices/ChoiceButton.test.tsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+import {render} from '@testing-library/react';
+import ChoiceButton from "./ChoiceButton";
+import BoomerGregorEvent from "../../events/BoomerGregorEvent";
+
+it("renders choices correctly", () => {
+    for (const c of new BoomerGregorEvent().choices()) {
+        const { getByText } = render(<ChoiceButton
+            choice={c}
+            makeChoice={() => {}} />);
+        expect(getByText(c.answer())).toBeInTheDocument();
+    }
+});
