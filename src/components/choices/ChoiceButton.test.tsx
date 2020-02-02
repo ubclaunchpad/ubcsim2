39cd12c8c1d06616e07b9c328a4c4091b5794c88
Cheuk Yin Ng <chucksys88@gmail.com>
2020-02-01 16:11:33 -0800

    Silence the linter

--- src/components/choices/ChoiceButton.test.tsx
+++ src/components/choices/ChoiceButton.test.tsx
@@ -1,14 +1,17 @@
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import {render} from '@testing-library/react';
+import {render} from "@testing-library/react";
 import ChoiceButton from "./ChoiceButton";
 import BoomerGregorEvent from "../../events/BoomerGregorEvent";
 
 it("renders choices correctly", () => {
     for (const c of new BoomerGregorEvent().choices()) {
-        const { getByText } = render(<ChoiceButton
-            choice={c}
-            makeChoice={() => {}} />);
+        const { getByText } = render(
+            <ChoiceButton
+                choice={c}
+                makeChoice={() => {}}
+            />
+        );
         expect(getByText(c.answer())).toBeInTheDocument();
     }
 });
