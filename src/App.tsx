dbe7271e977586dfbd1741ab8f4d90cea925e015
Robin Reyes <rmcreyes.eng@gmail.com>
2020-02-06 19:07:07 -0800

    Add unity content

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
 import React from "react";
+import Unity, { UnityContent } from "react-unity-webgl";
+
+const unityContent1 = new UnityContent(
+    "buildplacething/Build/buildplacething.json",
+    "buildplacething/Build/UnityLoader.js"
+)
+
+const unityContent2 = new UnityContent(
+    "unity_builds/Build/unity_builds.json",
+    "unity_builds/Build/UnityLoader.js"
+)
 
 export default class App extends React.Component {
 
@@ -6,6 +17,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <h1>HELLO WORLD</h1>
+                <Unity unityContent={unityContent1} />
             </div>
         );
     }
