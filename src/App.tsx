dc50dbb0302dab168cf676983131cfd44ade4a88
Robin Reyes <rmcreyes.eng@gmail.com>
2020-02-07 21:40:23 -0800

    Test reloading Unity player

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,51 @@
 import React from "react";
 import Unity, { UnityContent } from "react-unity-webgl";
 
-const unityContent1 = new UnityContent(
-    "buildplacething/Build/buildplacething.json",
-    "buildplacething/Build/UnityLoader.js"
-)
+// const unityContent1 = new UnityContent(
+//     "buildplacething/Build/buildplacething.json",
+//     "buildplacething/Build/UnityLoader.js"
+// )
 
 const unityContent2 = new UnityContent(
     "unity_builds/Build/unity_builds.json",
     "unity_builds/Build/UnityLoader.js"
 )
 
-export default class App extends React.Component {
+export interface IProps {}
+
+interface IState {
+    current_i: boolean;
+}
+
+export default class App extends React.Component <IProps, IState> {
+
+    constructor(props: IProps) {
+        super(props);
+        this.state = {current_i: true};
+    }
+
+    changeGame = () => {
+        this.setState(prevState => {
+            return {
+                current_i: prevState.current_i ? false : true
+            };
+        });
+    }
 
     render() {
+        if (this.state.current_i) console.log("RENDERING - current_i: true");
+        else console.log("RENDERING - current_i: false");
         return (
             <div>
                 <h1>HELLO WORLD</h1>
-                <Unity unityContent={unityContent1} />
+                <button
+                    onClick={() => this.changeGame()}
+                >
+                    {"Click me!"}
+                </button>
+                {/* <Unity unityContent={this.state.current_i ? unityContent2 : unityContent1} /> */}
+                <h1>{this.state.current_i ? "TRUE" : "FALSE"}</h1>
+                {this.state.current_i ? <Unity unityContent={unityContent2} /> : null}
             </div>
         );
     }
