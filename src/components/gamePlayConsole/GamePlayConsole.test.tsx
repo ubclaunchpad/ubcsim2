f9027fb32a4ae7862e86c8c1a37cc54fe54a6e75
Cheuk Yin Ng <chucksys88@gmail.com>
2020-02-08 15:07:31 -0800

    WIP add GPC test

---
+++ src/components/gamePlayConsole/GamePlayConsole.test.tsx
@@ -0,0 +1,28 @@
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import {render} from "@testing-library/react";
+import GamePlayConsole from "./GamePlayConsole";
+import { GamePlayMode } from "../../events/core";
+
+it("renders acceptance letter when asked to", () => {
+    const {getByText} = render(
+        <GamePlayConsole
+            mode={GamePlayMode.AcceptanceLetter}
+            imgPath="something.png"
+        />
+    );
+
+    expect(getByText("Congratulations! Please accept this offer of admission to UBC.")).toBeInTheDocument();
+    {/* expect(getByText("Please accept this offer")).toBeInTheDocument(); */}
+});
+
+it("renders comic when asked to", () => {
+    const {getByText} = render(
+        <GamePlayConsole
+            mode={GamePlayMode.Comics}
+            imgPath="someting.png"
+        />
+    );
+
+    expect(getByText("Please accept this offer")).not.toBeInTheDocument();
+});
