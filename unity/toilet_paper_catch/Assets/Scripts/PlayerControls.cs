4dad6d7d0d3402ff8356dc5ea30235be0f396655
Robin Reyes <rmcreyes.eng@gmail.com>
2020-03-18 23:25:58 -0700

    Add horizontal movement to player

---
+++ unity/toilet_paper_catch/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveHorizontal, 0) * moveSpeed;
+    }
+}
