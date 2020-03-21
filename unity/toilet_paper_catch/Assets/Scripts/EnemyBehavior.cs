4f52007243503c5fe6434b8a66f614431edfd4b4
Robin Reyes <rmcreyes.eng@gmail.com>
2020-03-20 23:15:17 -0700

    Add enemy behavior

---
+++ unity/toilet_paper_catch/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
