a6e35c3b7c0f38fa89464128f49c774516b0a53c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-01 23:04:02 +0000

    Deploy ubclaunchpad/ubcsim2 to github.com/ubclaunchpad/ubcsim2.git:gh-pages

---
+++ precache-manifest.96e182d46f35a38fa592cd3ffbad700a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d111bd488dbda982691dd20b5a020b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ccb23744fb0467a4e8d",
+    "url": "/static/js/2.db9614d2.chunk.js"
+  },
+  {
+    "revision": "db75550329f3dc8f6956",
+    "url": "/static/js/main.d75e454e.chunk.js"
+  },
+  {
+    "revision": "a00a9e819a8f9ee1b373",
+    "url": "/static/js/runtime-main.fd4528e6.js"
+  },
+  {
+    "revision": "5eb701e5096283966162a122fa37799d",
+    "url": "/static/media/abg_boyfriend.5eb701e5.png"
+  },
+  {
+    "revision": "e58d9c986a33189b6ed4db64591b3dea",
+    "url": "/static/media/abg_no_airpods.e58d9c98.png"
+  },
+  {
+    "revision": "f7288c7264f435818be854ec33043b19",
+    "url": "/static/media/abg_normal.f7288c72.png"
+  },
+  {
+    "revision": "1262121dcaaa1c85b8199b1d50331c0c",
+    "url": "/static/media/abg_sad.1262121d.png"
+  },
+  {
+    "revision": "2d5686322f96a55163a50ce4077f117d",
+    "url": "/static/media/corona.2d568632.png"
+  },
+  {
+    "revision": "e362e7f2ab3f9e3c06a19e9bdfe0060e",
+    "url": "/static/media/imagineDay.e362e7f2.png"
+  },
+  {
+    "revision": "4f524b23611f96726f7c3c716767fea8",
+    "url": "/static/media/longboat.4f524b23.png"
+  },
+  {
+    "revision": "1f309422a493e7cc0c6706471b9e545c",
+    "url": "/static/media/losing.1f309422.png"
+  },
+  {
+    "revision": "841b2939ddc76389e470c66fedad304d",
+    "url": "/static/media/midtermPaper.841b2939.png"
+  },
+  {
+    "revision": "fd8094cc2d4c50eacfdd7db3c97c6113",
+    "url": "/static/media/place-holder-image.fd8094cc.png"
+  },
+  {
+    "revision": "b15b15dd515022bbee1e3fb51ebb7fba",
+    "url": "/static/media/stormTheWall.b15b15dd.png"
+  },
+  {
+    "revision": "8509c588549751f64db10f7fabc1640c",
+    "url": "/static/media/toga.8509c588.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ubcsim2/precache-manifest.2c619af75859d7e4e63874c1a270bb95.js"
+  "/precache-manifest.96e182d46f35a38fa592cd3ffbad700a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ubcsim2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
